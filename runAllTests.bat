29bed7185aab3ed88cf827e659509b9839e5374c
Seth Brodzik <sethmb83@gmail.com>
2021-11-22 10:37:13 -0500

    Update runAllTests.bat
    DEMO_MODE variable properly set, other unnecessary code cleaned up.  May need to find environment name automatically so this file works for everyone's directory automatically.

--- runAllTests.bat
+++ runAllTests.bat
@@ -1,33 +1,32 @@
-CALL activate JEC
-cd C:\Users\Seth\Documents\GitHub\Attack-on-Co-Processors\
+@echo on
+::NOTE: SPACES MATTER, FORMAT EXACTLY AS DESCRIBED HERE
 
-::@echo OFF
-
-:: Define Path to Conda Installation and Environment Name so user input isn't needed
-set CONDAPATH=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)
+:: Conda Environment Name
 set ENVNAME=JEC
-:: Demo Flag. If "True", a single image will be tested for each non-ideality rather than the whole image set. 
-set demo_mode = FALSE
-PAUSE
+:: Demo Flag. If TRUE, a single image will be tested for each non-ideality rather than the whole image set. 
+set DEMO_MODE=TRUE
+
+CALL activate %ENVNAME%
+
 :: Run a python script in that environment
-::if %demo_mode% == TRUE (
-::	python testing.py -d
-::	python testing.py -dD
-::	python testing.py -dE
-::	python testing.py -dR
-::	python testing.py -dF
-::	python testing.py -dDE
-::	python testing.py -dDR
-::	python testing.py -dDF
-::	python testing.py -dER
-::	python testing.py -dEF
-::	python testing.py -dRF
-::	python testing.py -dDER
-::	python testing.py -dDEF
-::	python testing.py -dDRF
-::	python testing.py -dERF
-::	python testing.py -dDERF
-::) else (
+if %DEMO_MODE%==TRUE ( 
+	python testing.py -d
+	python testing.py -dD
+	python testing.py -dE
+	python testing.py -dR
+	python testing.py -dF
+	python testing.py -dDE
+	python testing.py -dDR
+	python testing.py -dDF
+	python testing.py -dER
+	python testing.py -dEF
+	python testing.py -dRF
+	python testing.py -dDER
+	python testing.py -dDEF
+	python testing.py -dDRF
+	python testing.py -dERF
+	python testing.py -dDERF
+) else (
 	python testing.py 
 	python testing.py -D
 	python testing.py -E
@@ -44,9 +43,10 @@ PAUSE
 	python testing.py -DRF
 	python testing.py -ERF
 	python testing.py -DERF
-::)
+)
 
 :: Deactivate the environment
 call conda deactivate 
+echo Testing Completed.
 
 PAUSE
