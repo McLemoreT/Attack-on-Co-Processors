f04d213ca442044bd4f4a461a6a852477b44475f
Robert Sunbury <sunrobbie00@gmail.com>
2022-04-14 14:29:36 -0400

    Added test code for bore.py in tests.py

--- tests.py
+++ tests.py
@@ -13,9 +13,11 @@ import deepfool
 #Other Files
 from TorchUtils import goodPerturb
 import explorer as explorer
+import bore as borer
+import matplotlib.pyplot as plt
     
 
-def QuarryTest(polyset, model, patchedModel, fool_set):
+def QuarryTest(model, patchedModel, fool_set):
     counter = 0
     isGoodPlace_Loader = torch.utils.data.DataLoader(
         fool_set, batch_size=100, shuffle=True, num_workers=8
@@ -42,3 +44,23 @@ def goodPerturbTest(fool_set, model, patchedModel):
         for i in range(0, len(label)):
             
             good_data.append(goodPerturb(model, patchedModel, images[i], label[i]))
+            
+def boreTest(model, patchedModel, fool_set):
+    counter = 0
+    isGoodPlace_Loader = torch.utils.data.DataLoader(
+        fool_set, batch_size=100, shuffle=True, num_workers=8
+    )
+    while counter < 5: #Number of batches to go through
+        images, label = next(iter(isGoodPlace_Loader)) #A loader iterator returns a tensor of images, and their
+                                                #labels
+        for i in range(0, len(label)):
+            actual_class, label_software, label_memristor, count, hash_val, pert_image = goodPerturb(model, patchedModel, images[i], label[i])
+           
+            if pert_image is not None:
+                pert_image = explorer.quarry.rankFix(pert_image)
+                
+                goodSet = borer.bore(model, actual_class, patchedModel, pert_image, count=50)
+                
+                for x in goodSet:
+                    fileName = str(actual_class) + "_" + str(label_software) + "_" + hash(x)
+                    plt.imsave(fileName, x.reshape((x.size(dim=2), x.size(dim=2))))
